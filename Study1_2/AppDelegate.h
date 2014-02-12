0cb00c22df5d5e6f310d62499c98cf6ab085bf0a
reedfim <reedfim@gmail.com>
2014-02-12 21:38:34 +0900

    Initial Commit

---
+++ Study1_2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Study1_2
+//
+//  Created by reedfim on 2014. 2. 12..
+//  Copyright (c) 2014년 reedfim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Study1_2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Study1_2
+//
+//  Created by reedfim on 2014. 2. 12..
+//  Copyright (c) 2014년 reedfim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
